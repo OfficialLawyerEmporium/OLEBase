ad6bb23d62f4cbce950927ec9b843fb77d8da960
OfficialLawEmpire <79274984+OfficialLawyerEmporium@users.noreply.github.com>
2021-07-06 18:08:07 -0400

    @theexyzpi: JudgeSOJHD

--- characters/EmaSOJHD/(b)/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for filename in os.listdir():
-    print(filename)
-    if filename.endswith(".gif"):
-        newname = filename.split(')')[1]
-        os.rename(filename, newname)
-        print("success " + newname)
